46bbd88b46eca90c469d460490daf96bfd8ab581
Moritz Schönherr <moritz.schoenherr@gmail.com>
2018-02-07 16:29:19 +0100

    feat: filter button

--- index.js
+++ index.js
@@ -19,19 +19,27 @@ function Place (name, placeid, location) {
 function PlacesModel () {
     var self = this;
 
+    // currentInput holds the string by which the places are filtered
     self.currentInput = ko.observable("");
     
+    // This array of places could be loaded from a server.
     self.places = [
 	new Place("Somewhere","XXX", "here"),
 	new Place("Elsewhere", "XXX", "there")
     ];
 
+    // filteredPlaces is a computed observable returning the places
+    // for which place.name includes the currentInput
     self.filteredPlaces = ko.computed(function () {
 	return _.filter(self.places,
 			function (place) {
 			    return place.name.includes(self.currentInput());
 			});
     });
+
+    self.filterMarkers = function () {
+	console.log("code to filter markers");
+    }
 
 }
 
